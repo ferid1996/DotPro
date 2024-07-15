5e1f70d2b5a4b81a8ebdeb6333aae4587c6d1743
Ferid <rzazadeferid72@gmail.com>
2024-07-15 11:10:41 +0400

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NG.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
